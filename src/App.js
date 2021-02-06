90d9eb05859cf0a9644d68b68d3fb51cc9a43386
katrine-r <katrinegantsova@gmail.com>
2021-02-06 22:02:40 +0200

    added car array and title changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,49 @@ import Car from "./Car/Car";
 
 class App extends Component {
   
+  state = {
+    cars: [
+      {name: 'Ford', year: '2018'},
+      {name: 'Audi', year: '2016'},
+      {name: 'Mazda', year: '2010'}
+    ],
+    pageTitle: 'React component'
+  }
+
+  changeTitleHandler = (newTitle) => { 
+    this.setState({
+      pageTitle: newTitle
+    })
+  }
+
   render() {
     const divStyle = {
       textAlign: "center"
     };
 
+    const cars = this.state.cars
+
     return (
       <div style={divStyle}>
-        <h1>Hello World</h1>
+        <h1>{this.state.pageTitle}</h1>
 
-        <Car name={'Ford'} year={2018} />
-        <Car name={'Audi'} year={2016} />
-        <Car name={'Mazda'} year={2010} />
+        <button onClick={this.changeTitleHandler.bind(this, ' Change')}>Change title</button>
+
+        <Car 
+          name={cars[0].name} 
+          year={cars[0].year} 
+          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
+        />
+        <Car 
+          name={cars[1].name} 
+          year={cars[1].year} 
+          onChangeTitle={this.changeTitleHandler.bind(this, cars[1].name)}
+        />
+        <Car 
+          name={cars[2].name} 
+          year={cars[2].year} 
+          onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+        />
       </div>
     );
   }
