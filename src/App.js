4528d32867203c2b81e721e58bd661f9a8960bc5
katrine-r <katrinegantsova@gmail.com>
2021-02-08 22:22:09 +0200

    create dynamic list, changed name, deleted element

--- src/App.js
+++ src/App.js
@@ -14,14 +14,27 @@ class App extends Component {
     showCars: false
   }
 
-  changeTitleHandler = (newTitle) => { 
-    this.setState({
-      pageTitle: newTitle
-    })
-  }
   toggleCarsHandler = () => {
     this.setState({
       showCars: !this.state.showCars
+    })
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({
+      cars
+    })
+  }
+
+  deleteHandler = (index) => {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+    this.setState({
+      cars
     })
   }
 
@@ -30,25 +43,30 @@ class App extends Component {
       textAlign: "center"
     };
 
+    let cars = null
+
+      if (this.state.showCars) {
+        cars = this.state.cars.map((car, index) => {
+          return (
+            <Car
+              key={index}
+              name={car.name}
+              year={car.year}
+              onDelete={this.deleteHandler.bind(this, index)}
+              onChangeName={(event) => this.onChangeName(event.target.value, index)}
+            />
+          )
+        }
+        )
+      } 
+        
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
         <button onClick={this.toggleCarsHandler}>Toggle cars</button>
 
-        { this.state.showCars ?
-        this.state.cars.map((car, index) => {
-          return (
-            <Car
-              key={index}
-              name={car.name}
-              year={car.year}
-              onChangeTitle={() => this.changeTitleHandler(car.name, index)}
-            />
-          )
-        }
-        ) : null
-      }
+        { cars }
 
       </div>
     );
